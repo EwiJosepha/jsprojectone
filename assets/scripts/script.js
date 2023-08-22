d910dc464fdb7280f51a08dee3c2f70b5ae2f3cb
ewi josepha <ewijosepha@gmail.com>
2023-08-22 16:18:15 +0200

    summing numbers

---
+++ assets/scripts/script.js
@@ -0,0 +1,18 @@
+//DOM manipulation
+// const number_1 = document.getElementById("number-1");
+// const number_2 = document.getElementById("number-2");
+// const add = document.getElementById("add");
+
+// const check = document.getElementById("check");
+
+// console.log("numer1", number_1.value);
+// console.log("number", number_2.value);
+
+// const arrowFucntino = () => {
+//   const result = parseInt(number_1.value) + parseInt(number_2.value);
+//   console.log(typeof parseInt(number_1.value), typeof number_2.value);
+
+//   add.value = result;
+// };
+// //add.value(id)
+// check.addEventListener("click", arrowFucntino);
